2dfe898c49d3bc26b48d311eebf6eb7ab967212c
Jinky Macandili <jinkymacandili05@gmail.com>
2026-09-10 15:58:14 +0800

    crud

--- app/config/database.php
+++ app/config/database.php
@@ -58,13 +58,13 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 */
 
 $database['main'] = array(
-    'driver'    => '',
+    'driver'    => 'mysql',
     'hostname'  => getenv('DB_HOST') ?: '',
     'port'      => getenv('DB_PORT') ?: '',
     'username'  => getenv('DB_USERNAME') ?: '',
     'password'  => getenv('DB_PASSWORD') ?: '',
-    'database'  => getenv('DB_NAME') ?: 'mydb',
-    'charset'   => getenv('DB_CHARSET') ?: 'utf8mb4',
+    'database'  => getenv('DB_NAME') ?: '',
+    'charset'   => getenv('DB_CHARSET') ?: '',
     'dbprefix'  => getenv('DB_PREFIX') ?:  '',
     'path'      => ''
 );
